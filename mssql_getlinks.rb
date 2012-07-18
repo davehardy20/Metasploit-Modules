52c4a3458234278620c84f91c7c93464d049c7ee
nullbind <scott.sutherland@nullbind.com>
2012-07-17 22:01:08 -0500

    updates

--- mssql_getlinks.rb
+++ mssql_getlinks.rb
@@ -23,25 +23,31 @@ class Metasploit3 < Msf::Auxiliary
 			], self.class)
 	end
 
+	
 	def print_with_underline(str)
 		print_line(str)
 		print_line("=" * str.length)
 	end
 
+	
 	def run_host(ip)
 		
 		# Set verbosity
 		verbose = datastore['verbose'].to_s.downcase 
 		
+		# Crate primary array
+		links_to_crawl= Array.new 	
+		links_crawled= Array.new 	
+		
 		# Setup first server
-		links_to_crawl = getstartdata(verbose)
+		links_to_crawl = getstartdata(ip,links_to_crawl,verbose)
 		
 		# Debugging information
 		print_status("Hash exported by first method:") if verbose == "true"
 		links_to_crawl.each do |link| print_status("#{link}") end if verbose == "true"	
 		
 		# Crawl database links
-		crawl_db_links(links_to_crawl,verbose)
+		crawl_db_links(links_to_crawl,links_crawled,verbose)
 			
 	end
 	
@@ -49,10 +55,7 @@ class Metasploit3 < Msf::Auxiliary
 	##
 	## Method of getting initial data from target SQL Server
 	##
-	def getstartdata(verbose)
-	
-		# CREAT ARRAY TO STORE DB SERVER HASH
-		links_to_crawl= Array.new 	#LINKS TO CRAWL
+	def getstartdata(ip,links_to_crawl,verbose)
 			
 		# GET INFORMATION FROM CURRENT DATABASE
 		# SETUP QUERY
@@ -65,6 +68,7 @@ class Metasploit3 < Msf::Auxiliary
 			@@servername as linkpath"
 		
 		##	EXECUTE QUERY
+		print_status("Attempting to obtain data from SQL Server at '#{ip}'...")
 		result = mssql_query(sql, false) if mssql_login_datastore
 		column_data = result[:rows]
 		
@@ -77,6 +81,8 @@ class Metasploit3 < Msf::Auxiliary
 		}
 		disconnect
 		
+		print_good("Successfully obtained data from SQL Server at '#{ip}'...")
+		
 		# Debugging information	
 		print_status("Hash defined in first method for initial target:") if verbose == "true"
 		links_to_crawl.each do |link| print_status("#{link}") end if verbose == "true"		
@@ -85,10 +91,11 @@ class Metasploit3 < Msf::Auxiliary
 		return links_to_crawl
 	end
 	
+	
 	##
-	## Method (recursive) for crawling database links
+	## Method for crawling database links
 	##
-	def crawl_db_links(links_to_crawl,verbose)
+	def crawl_db_links(links_to_crawl,links_crawled,verbose)
 		
 		# Debugging information
 		print_status("Hash imported to recursive function from first method:") if verbose == "true"
@@ -101,7 +108,12 @@ class Metasploit3 < Msf::Auxiliary
 			print_status("Attempting to crawl database links...")
 			
 			# Iterate through each hash
-			
+			links_to_crawl.map {|dbsrv_hash| 						
+				
+				# Statusing
+				print_status("---------------------------------------------------------------")
+				print_status("Processing: #{dbsrv_hash['server']} : #{dbsrv_hash['sysadmin']} : #{dbsrv_hash['version']} : #{dbsrv_hash['linkpath']} : #{dbsrv_hash['parent']}")				
+				
 				# Set path seperator character
 				
 				# Set target server/path to be processed
@@ -124,12 +136,45 @@ class Metasploit3 < Msf::Auxiliary
 						
 						# If link does not exist in links_to_crawl or links_crawled then
 						
-							# Add link to links_to_crawl if it does not exist
+							
 							
 							#Update link hash info using data from sql query 
 				
-				# Remove current target from links_to_crawl
-
+				# Add current target server to links_crawled
+				temp_hash = Hash['server'=>"#{dbsrv_hash['server']}",'sysadmin'=>"#{dbsrv_hash['sysadmin']}",'version'=>"#{dbsrv_hash['version']}",'linkpath'=>"#{dbsrv_hash['linkpath']}",'parent'=>"#{dbsrv_hash['parent']}"]
+				links_crawled << temp_hash
+				
+				# Debugging information	
+				print_status("crawled links:") if verbose == "true"
+				links_crawled.each do |link| print_status("link: #{link}") end if verbose == "true"	
+				
+				# Remove current target server from links_to_crawl
+				#links_crawled.del if dbs_to_crawl.include? "#{linkpath}"	
+				print_status("linkpath: #{dbsrv_hash['linkpath']}")
+				links_to_crawl.each {|serverinfo,servervalue|	
+					
+					print_good("server info: #{serverinfo}, Value: #{servervalue}")
+				
+					serverinfo.each {|key,value|
+						if key == "linkpath" and value == "#{dbsrv_hash['linkpath']}" then
+							print_status("KEY: #{key}, Value: #{value}")
+							
+							# Setup links_to_crawl key to be deleted
+							
+							
+							# Delete links_to_crawl key
+							
+							# or delete the current (first) entry in the hash array
+							
+						end
+					}
+				}
+				
+				# Debugging information	
+				print_status("Links to crawl:") if verbose == "true"
+				links_to_crawl.each do |link| print_status("link: #{link}") end if verbose == "true"	
+			}
+			
 			# crawl_db_links(links_to_crawl) if links_to_crawl is not empty
 		end
 	end
